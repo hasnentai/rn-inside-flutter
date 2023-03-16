b7ff790deb23057c5a9c4c986117450cf6992225
hasnentai <hasnen712@live.com>
2023-03-16 23:37:17 +0530

    working on older version of react native

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { AppRegistry, Text, View } from "react-native";
+
+const Integration = () => {
+  return (
+    <View >
+      <Text >Hello, World</Text>
+    </View>
+  );
+};
+
+
+AppRegistry.registerComponent("IntegratedApp", () => Integration);
